037368e2fe751a8cc86ed7b369a1855718f895ab
Huy <lexuanhuy8695@gmail.com>
2023-08-03 18:03:55 +0700

    aaaa

--- src/components/products/modalCreatedProduct.js
+++ src/components/products/modalCreatedProduct.js
@@ -612,7 +612,7 @@ const CreateNewProduct = (props) => {
         formData.append("order", userdata.order);
 
 
-        fetch("https://huyle-backend-app.onrender.comapi/v6/upload-multiple-pic", {
+        fetch("https://huyle-backend-app.onrender.com/api/v6/upload-multiple-pic", {
             method: "POST",
             body: formData,
 
